1fd129b54eb4497c445a3c52435584384c5b0202
Primexus <pierre.tran@ymail.com>
2021-09-14 12:22:30 -0400

    Exercice 1 is done

--- Test.py
+++ Test.py
@@ -1,6 +1,9 @@
-def fizzBuzz(n):
-    # TODO imprimer la chaine de caractère appropriée avec la fonction print().
-    #  Assigner ensuite la valeur à la variable resultat
-    n = float(input("Donnez un nombre n :" ))
-    resultat =
-    return resultat
+n = int(input("Enter a number : "))
+if n % 3 == 0:
+    if n % 5 == 0:
+        print("fizzbuzz")
+    else:
+        print("fizz")
+else:
+    if n % 5 == 0:
+        print("buzz")
--- exercice1.py
+++ exercice1.py
@@ -1,12 +1,18 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
-    n = input("Donnez un nombre n :" )
-    if n%3 == 0:
-        print("fizz")
-    if n%5 == 0:
-        print("buzz")
-    resultat =
+    if n % 3 == 0:
+        if n % 5 == 0:
+            type1 = "fizzbuzz"
+            return type1
+        else:
+            type3 = "fizz"
+            return type3
+    else:
+        if n % 5 == 0:
+            type2 = "buzz"
+            return type2
+    resultat = n
     return resultat
 
 if __name__ == '__main__':
