9abe5438d6d8c416e042b1959fe31ebd0f64177a
Primexus <pierre.tran@ymail.com>
2021-09-14 11:24:25 -0400

    commit test modif 1

---
+++ Test.py
@@ -0,0 +1,6 @@
+def fizzBuzz(n):
+    # TODO imprimer la chaine de caractère appropriée avec la fonction print().
+    #  Assigner ensuite la valeur à la variable resultat
+    n = float(input("Donnez un nombre n :" ))
+    resultat =
+    return resultat
--- exercice1.py
+++ exercice1.py
@@ -1,7 +1,11 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
-
+    n = input("Donnez un nombre n :" )
+    if n%3 == 0:
+        print("fizz")
+    if n%5 == 0:
+        print("buzz")
     resultat =
     return resultat
 
